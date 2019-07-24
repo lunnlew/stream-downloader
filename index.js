c8d5041be96e8d32daf6ac14905cadcf6f407ee5
lunnlew <lunnlew@qq.com>
2019-07-24 15:50:38 +0800

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+'use strict';
+const path = require('path')
+const usage = require(path.join(__dirname,'./lib/usage'))
+const streamdl = require(path.join(__dirname,'./lib/stream-dl'))
+
+streamdl.exec(usage.optsParser(process.argv, usage.parsePackageInfo()))
