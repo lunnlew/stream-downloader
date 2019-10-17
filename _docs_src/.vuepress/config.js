bfdb076d9911b328200a53811bc494051b2c22da
lunnlew <lunnlew@qq.com>
2019-10-17 23:35:26 +0800

    add doc

---
+++ _docs_src/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    base: '/stream-downloader/',
+    title: 'Stream Downloader',
+    dest: 'docs',
+    description: 'Stream Downloader, 是一个用于从网络下载媒体资源(视频，音频，图片等)的工具。',
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            { text: '指南', link: '/guide/' }
+        ]
+    }
+}
---
+++ _docs_src/README.md
@@ -0,0 +1,11 @@
+---
+home: true
+heroImage: /hero.png
+heroText: Stream Downloader
+tagline: 从网络下载媒体资源(视频，音频，图片等)的工具
+actionText: 快速上手 →
+actionLink: /guide/
+features:
+- title: 视频下载
+  details: 享受从网络下载媒体资源(视频，音频，图片等)的方便体验。
+---
